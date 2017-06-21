5d324830a798825eafa2fbcaae5cef40e11af1cf
Jonathan Guthrie <jono@perfect.org>
2017-06-21 14:53:41 -0400

    update minor versions

--- Package.swift
+++ Package.swift
@@ -7,6 +7,6 @@ let package = Package(
     dependencies: [
         .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 2),
         .Package(url: "https://github.com/PerfectlySoft/Perfect-Logger.git", majorVersion: 1),
-        .Package(url: "https://github.com/iamjono/SwiftString.git", majorVersion: 1),
+        .Package(url: "https://github.com/iamjono/SwiftString.git", majorVersion: 2),
     ]
 )
