95f8b84bedc42bd5771868c7ad4b5e568392b5c2
Jonathan Guthrie <jono@perfect.org>
2017-11-05 16:40:29 -0500

    update to swift 4, perfect 3

--- Package.swift
+++ Package.swift
@@ -1,12 +1,12 @@
 // Generated automatically by Perfect Assistant Application
-// Date: 2017-02-02 05:28:41 +0000
+// Date: 2017-11-05 21:32:55 +0000
 import PackageDescription
 let package = Package(
-    name: "PerfectWebRedirects",
-    targets: [],
-    dependencies: [
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 2),
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-Logger.git", majorVersion: 1),
-        .Package(url: "https://github.com/iamjono/SwiftString.git", majorVersion: 2),
-    ]
+	name: "PerfectWebRedirects",
+	targets: [],
+	dependencies: [
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 3),
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-Logger.git", majorVersion: 3),
+		.Package(url: "https://github.com/iamjono/SwiftString.git", majorVersion: 2),
+	]
 )
